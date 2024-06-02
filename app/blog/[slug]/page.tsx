7ddef9c6a1d102d94e3bc6b827e49340343f43c1
SebastianLib <sebastianlib04@gmail.com>
2024-06-02 14:10:26 +0200

    test

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,65 @@
+import { fullBlog } from "@/app/lib/interface";
+import { client, urlFor } from "@/app/lib/sanity";
+import { PortableText } from "@portabletext/react";
+import Image from "next/image";
+
+export const revalidate = 30; // revalidate at most 30 seconds
+
+export const generateStaticParams = async () => {
+  const slugs = await client.fetch("*[_type == 'blog'] { 'slug': slug.current }");
+  return slugs.map(( {slug}:{slug:string} ) => ({
+    slug: slug,
+  }))
+};
+
+async function getData(slug: string) {
+  const query = `
+    *[_type == "blog" && slug.current == '${slug}'] {
+        "currentSlug": slug.current,
+          title,
+          content,
+          titleImage
+      }[0]`;
+
+  const data = await client.fetch(query);
+  return data;
+}
+
+export default async function BlogArticle({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const data: fullBlog = await getData(params.slug);
+
+  // Check if data.title exists before rendering
+  if (!data || !data.title) {
+    return <div>Error: Blog not found</div>;
+  }
+
+  return (
+    <div className="mt-8">
+      <h1>
+        <span className="block text-base text-center text-primary font-semibold tracking-wide uppercase">
+          Jan Marshal - Blog
+        </span>
+        <span className="mt-2 block text-3xl text-center leading-8 font-bold tracking-tight sm:text-4xl">
+          {data.title}
+        </span>
+      </h1>
+
+      <Image
+        src={urlFor(data.titleImage).url()}
+        width={800}
+        height={800}
+        alt="Title Image"
+        priority
+        className="rounded-lg mt-8 border"
+      />
+
+      <div className="mt-16 prose prose-blue prose-lg dark:prose-invert prose-li:marker:text-primary prose-a:text-primary">
+        <PortableText value={data.content} />
+      </div>
+    </div>
+  );
+}
